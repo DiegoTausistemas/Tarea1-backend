a4007653068a167c66507a9eff19ada0ae20f4e5
Diego Jimenez <Diego@tausistemas.cl>
2020-10-01 19:37:26 -0300

    app: Creacion de servidor

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+// Importacion Dependencias
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+require('dotenv').config();
+
+
+// app express
+const app = express();
+
+
+// Ejecutamos conexión a MongoDB
+const dataBase = require('./lib/mongoose').db;
+dataBase();
+
+// Middleware
+app.use(morgan('dev'));
+
+
+// Puerto
+const port = process.env.PORT || 8000;        //--> si no toma el puerto de la DB toma por defecto el que le asigno yo
+
+
+//Listen port
+app.listen(port, () => {
+    console.log(`Servidor iniciado en puerto: ${port}`);
+});
